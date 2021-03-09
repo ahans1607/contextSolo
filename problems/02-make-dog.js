39f41e9b9917307e049354f9ffc5e8f9a4e54ee2
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-03-08 21:48:29 -0600

    02

--- problems/02-make-dog.js
+++ problems/02-make-dog.js
@@ -8,19 +8,6 @@ The speak function will intake a word and then return a sentence with the name
 of the dog saying that word. The changeName function will intake a newName and
 will set the dog object's name to be the passed in name. See below for examples:
 
-
-Examples:
-let dog1 = Dog.makeJet(); // returns an object 
-
-console.log(dog1.name); // Jet
-console.log(dog1.speak("hello")); // Jet says hello
-console.log(dog1.changeName("Freyja")); // Freyja
-console.log(dog1.name); // Freyja
-console.log(dog1.speak("hello")); // Freyja says hello
-
-let dog2 = Dog.makeJet();
-console.log(dog2.name); // Jet
-
 ***********************************************************************/
 
 function Dog(name) {
@@ -31,5 +18,25 @@ Dog.makeJet = function() {
 	return new this('Jet')
 }
 
+Dog.prototype.changeName = function(newName){
+	this.name = newName 
+	return newName
+}
+
+Dog.prototype.speak = function(word){
+	return this.name + " says " + word
+}
+
+
+let dog1 = Dog.makeJet(); // returns an object 
+
+console.log(dog1.name); // Jet
+console.log(dog1.speak("hello")); // Jet says hello
+console.log(dog1.changeName("Freyja")); // Freyja
+console.log(dog1.name); // Freyja
+console.log(dog1.speak("hello")); // Freyja says hello
+
+let dog2 = Dog.makeJet();
+console.log(dog2.name); // Jet
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = Dog;
